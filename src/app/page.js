4d266c487bfb606852fdd8a6852bfd6a76855ace
Sajal439 <23001001107sajal@dcrustm.org>
2025-03-27 10:39:25 +0530

    Add Radix UI Avatar component and update dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import MentorsPage from "./page/MentorsPage";
+import ProfilePage from "./page/ProfilePage";
 
 export default function Home() {
   return (
     <div>
-      <MentorsPage />
+      {/* <MentorsPage /> */}
+      <ProfilePage />
     </div>
   );
 }
