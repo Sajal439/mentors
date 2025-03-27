ae08e24a3d11f06967f191f1940fe171c1ef555e
ArchitGoyal2005 <architgoyal2005@gmail.com>
2025-03-27 21:57:28 +0530

    fixed layout of the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,25 @@
+import TopBar from "@/components/Layout/TopBar";
 import "./globals.css";
+import SideBar from "@/components/Layout/SideBar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex h-screen bg-white">
+          {/* Sidebar */}
+          <SideBar />
+
+          {/* Main content */}
+          <div className="flex-1 flex flex-col">
+            {/* Header */}
+            <TopBar />
+
+            {/* Content */}
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
